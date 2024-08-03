92de992ba75e2a2a3131ef685a4217a98605c353
Lautaro <lautarodm98@gmail.com>
2024-08-03 16:16:08 -0300

    url rss astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,7 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
-  site: 'https://example.com'
+
+  // site: 'https://example.com' - esto lo vemos al final en que dominio queda publicada nuestra app
+  site: 'https://lautaro-blog.netlify.app/'
 });
